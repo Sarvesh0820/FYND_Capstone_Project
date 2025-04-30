8dd71b71062ac9379c3b97c225f71e982c4cdf27
sarvesh0820 <sarveshhadgaonkar01@gmail.com>
2025-04-30 05:37:22 +0530

    `Added navigation functionality to Related component`

--- Frontend/src/components/Related.tsx
+++ Frontend/src/components/Related.tsx
@@ -2,10 +2,18 @@
 import { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import ProductItem from './ProductItem'
+import { useNavigate } from 'react-router-dom'
 
 const Related = ({ category, subCategory }) => {
     const { products } = useContext(ShopContext)
     const [related, setRelated] = useState([])
+    const navigate = useNavigate()
+
+
+    const handleClick = (id) => {
+        navigate(`/product/${id}`)
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
     
     useEffect(() => {
         if (products.length > 0) {
@@ -15,7 +23,9 @@ const Related = ({ category, subCategory }) => {
 
             setRelated(productsCopy.slice(0,5))
         }
-    },[products])
+    }, [products,category, subCategory])
+    
+
   return (
       <div className='my-24 '>
           <div className='text-center text-3xl py-2'>
@@ -24,7 +34,18 @@ const Related = ({ category, subCategory }) => {
           <div className='grid grid-cols-2 m:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
               {
                   related.map((item, index) => (
-                      <ProductItem key={index} id={item._id} name={item.name} price = {item.price} image={item.image} />
+                      <div
+            key={index}
+            onClick={() => handleClick(item._id)}
+            className='cursor-pointer'
+          >
+            <ProductItem
+              id={item._id}
+              name={item.name}
+              price={item.price}
+              image={item.image}
+            />
+          </div>
                   ))
               }
               
